592337b3f4b5dd57ceafaed096e6c8641a38d942
prasant.y <prasant.yadav@riskcovry.com>
2021-05-24 01:49:33 +0530

    gravatar_url resolution changed

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,7 @@
 class Room < ApplicationRecord
     has_many :room_messages, dependent: :destroy, inverse_of: :room
+
+    def as_json(options)
+        super(options).merge(user_avatar_url: user.gravatar_url)
+    end
 end
